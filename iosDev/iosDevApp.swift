f2397ccd2942a6097518e1f4f2ee58bc902fb6e6
Isabellebrian <Isabellebrian@gmail.com>
2023-09-12 20:42:41 +1000

    Initial Commit

---
+++ iosDev/iosDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosDevApp.swift
+//  iosDev
+//
+//  Created by Isabelle Brian on 12/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct iosDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
